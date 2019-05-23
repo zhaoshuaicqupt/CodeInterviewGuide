ce7a9333887266c25ecc4edb172d272e9b8fb8d8
zhaoshuai <zhaoshuaicqupt@163.com>
2019-05-23 12:03:47 +0800

    链表问题： 题目二： 在单链表和双链表中删除倒数第K个节点

---
+++ src/chapter_2_listproblem/Problem_02_DeleteLastKthNode.java
@@ -0,0 +1,14 @@
+package chapter_2_listproblem;
+
+public class Problem_02_DeleteLastKthNode {
+
+    public class Node{
+        public int val;
+        public Node node;
+        public Node(int val){
+            this.val=val;
+        }
+
+
+    }
+}
